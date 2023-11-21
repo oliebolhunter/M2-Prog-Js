9c7471ae1ceeb20984396b3740eedb0616b57158
jona <jona.wehnes@gmail.com>
2023-11-21 14:59:55 +0100

    nog meer veranderingen

--- 01A/app.js
+++ 01A/app.js
@@ -4,8 +4,20 @@ class App
     {
         console.log("hello world!");
         //code gaat hier
+    let appNaam = "dedikkewebsite"
+    let versienummer = 0.1
+    let versiedatum = 21-11-2023
+    let autheur = "Jona Wehnes"
+    let copyright = "de echte Jona"
+    let distributeur = "oliebol"
+    let darkmode = true
+        console.log("Appnaam:" + appNaam);
+        console.log("versiedatum:" + versiedatum);
+        console.log("Autheur" + autheur);
+        console.log("Copyright:" + copyright);
+        console.log("Distributeur:" + distributeur);
+        console.log("Darkmode:" + darkmode);
     }
-
 }
 
 let app = new App;
