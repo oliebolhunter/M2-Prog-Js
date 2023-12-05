c3641f3a204a8fa2561e65c7c0b6915eaa391a9d
jona <jona.wehnes@gmail.com>
2023-12-05 14:23:00 +0100

    04B

---
+++ 04B function/app.js
@@ -0,0 +1,36 @@
+function argumentsAreHandy(shoutout)
+{
+    console.log("do you want to give a shoutout?");
+    console.log(shoutout);
+
+}
+argumentsAreHandy("Ik Hou Van Frikandellen Met Mayonaise");
+
+argumentsAreHandy("Ik Hou Van Frikandellen Met Mayonaise");
+
+argumentsAreHandy("Ik Hou Van Frikandellen Met Mayonaise");
+
+function superMooieGlobalFunction()
+{
+    console.log("ik ben global")
+    console.log("dus je mag mij overal aanroepen")
+
+}
+
+superMooieGlobalFunction();
+
+superMooieGlobalFunction();
+
+superMooieGlobalFunction();
+
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!");
+        
+    }
+}
+
+let app = new App();
+app.runApplication();
